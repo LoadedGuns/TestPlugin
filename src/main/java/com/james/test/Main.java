a64f0f982526875711ef18bc09054212cbff121a
LoadedGuns <jsweetsir18@icloud.com>
2023-12-15 15:18:10 -0500

    start up

---
+++ src/main/java/com/james/test/Main.java
@@ -0,0 +1,18 @@
+package com.james.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("The plugin has started!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("The plugin has turned off!");
+    }
+}
