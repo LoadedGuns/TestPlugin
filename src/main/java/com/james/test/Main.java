b5a5cfb60eabae122958fd255af78c1d1445dfd1
LoadedGuns <jsweetsir18@icloud.com>
2023-12-15 15:42:54 -0500

    freeze/egg throw

--- src/main/java/com/james/test/Main.java
+++ src/main/java/com/james/test/Main.java
@@ -1,20 +1,37 @@
 package com.james.test;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
         System.out.println("The plugin has started!");
+
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+        e.setCancelled(true);
+        e.getPlayer().sendMessage(ChatColor.RED + "Stop moving!! You are frozen!!");
+    }
+
+    @EventHandler
+    public void onPlayerEggThrow(PlayerEggThrowEvent e) {
+        e.getPlayer().sendMessage(ChatColor.BLUE + "You just threw an egg!!");
+    }
+
+
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
-        System.out.println("The plugin has turned off!");
+        System.out.println("The plugin had an error!");
     }
 }
-
-
