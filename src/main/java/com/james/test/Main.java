279ba585e22fb0b2b54a59671e0a0e3911e1a6b1
LoadedGuns <jsweetsir18@icloud.com>
2023-12-16 20:45:57 -0500

    imports optimized

--- src/main/java/com/james/test/Main.java
+++ src/main/java/com/james/test/Main.java
@@ -2,11 +2,12 @@ package com.james.test;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin implements Listener {
